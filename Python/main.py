35fa2e7f527b5442b2af41329c47726e04255eef
Quentin <quentin.laurent78800@gmail.com>
2026-03-27 19:47:09 +0100

    mise à jour de main.py

--- Python/main.py
+++ Python/main.py
@@ -1,1 +1,18 @@
+import cv2 #On importe OpenCV(cv2)
 
+cap= cv2.VideoCapture(0) #On crée une variable cap qui récupère le flux video.
+
+while True: #On initie une boucle qui permet d'afficher chaque frame du flux vidéo. 
+
+    ret,frame=cap.read() 
+    ''' On crée une variable ret (booléen) qui renvoie True si cap enregistre un flux vidéo et une variable frame (numpy.ndarray) qui stocke l'information du flux vidéo. '''
+
+    if not ret: break  #On arrête la boucle si la variable ret vaut false. 
+
+    cv2.imshow('1',frame)  # On affiche l'image contenue dans frame dans une nouvelle fenêtre nommée "1" 
+
+    if cv2.waitKey(1) & 0xFF == ord('q'): break # On arrête la boucle si la touche "q" est pressée 
+
+
+cap.release() # la variable cap abandonne le contrôle de la camera pour ne pas bloquer les autres programmes 
+cv2.destroyAllWindows() # On ferme toutes les fenêtres ouvertes par le programme 
