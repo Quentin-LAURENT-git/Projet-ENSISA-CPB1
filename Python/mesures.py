61c2fa3cad16fbd8fc5051174e7bea804754c755
Quentin <quentin.laurent78800@gmail.com>
2026-04-02 12:25:18 +0200

    Update mesures.py

--- Python/mesures.py
+++ Python/mesures.py
@@ -1,6 +1,6 @@
 import cv2 
 
-"""Mesure du pérmiètre de l'onjet"""
+"""Mesure du pérmiètre de l'objet"""
 def perimetre(contours):
     for contour in contours :
         p = cv2.arcLength(contour, True)
