7587827e3c57e0ff03100069b310cc1b1b400e13
Quentin <quentin.laurent78800@gmail.com>
2026-05-15 12:36:11 +0200

    Ajout d'un ficher de setup pour windows

---
+++ setup_windows.ps1
@@ -0,0 +1,7 @@
+python -m venv venv_Projet_CPB1_ENSISA
+venv_Projet_CPB1_ENSISA\Scripts\Activate.ps1
+pip install --upgrade pip
+pip install numpy opencv-python numba spyder
+Write-Host "Environnement prêt."
+Write-Host "Pour lancer le projet : venv_Projet_CPB1_ENSISA\Scripts\Activate.ps1 puis spyder"
+
